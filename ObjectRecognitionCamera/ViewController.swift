fdf7f3beb6d352d586b4f2bb79e1c266d2a4efaa
a <ruan65@gmail.com>
2017-10-14 23:58:02 +0300

    change ml module

--- ObjectRecognitionCamera/ViewController.swift
+++ ObjectRecognitionCamera/ViewController.swift
@@ -15,37 +15,49 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let captureSession = AVCaptureSession()
-        captureSession.sessionPreset = .photo
+        if let session = setCaptureSession() {
+            setUi(session: session)
+        }
+    }
+    
+    private func setUi(session: AVCaptureSession) {
         
-        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-        
-        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
-        
-        captureSession.addInput(input)
-        
-        captureSession.startRunning()
-        
-        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        let previewLayer = AVCaptureVideoPreviewLayer(session: session)
         
         previewLayer.frame = view.frame
         
         view.layer.addSublayer(previewLayer)
+    }
+    
+    private func setCaptureSession() -> AVCaptureSession? {
+        
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return nil }
+        
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return nil }
+        
+        captureSession.addInput(input)
+        
+        captureSession.startRunning()
         
         let dataOutput = AVCaptureVideoDataOutput()
         
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "cameraQueue"))
         
         captureSession.addOutput(dataOutput)
+        
+        return captureSession
     }
 
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
         
-        let squeezeNetModel = SqueezeNet().model
+        let resNetModel = Resnet50().model
         
-        guard let model = try? VNCoreMLModel(for: squeezeNetModel) else { return }
+        guard let model = try? VNCoreMLModel(for: resNetModel) else { return }
         
         let request = VNCoreMLRequest(model: model) {
             

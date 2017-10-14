c4c8c95c4d895a8649cf895f19573a6e3f3154c0
a <ruan65@gmail.com>
2017-10-14 19:50:48 +0300

    use machine learning model for objects recognition

--- ObjectRecognitionCamera/ViewController.swift
+++ ObjectRecognitionCamera/ViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
         
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
         
@@ -23,9 +25,40 @@ class ViewController: UIViewController {
         captureSession.addInput(input)
         
         captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        
+        previewLayer.frame = view.frame
+        
+        view.layer.addSublayer(previewLayer)
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        
+        dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "cameraQueue"))
+        
+        captureSession.addOutput(dataOutput)
     }
 
-
-
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        let squeezeNetModel = SqueezeNet().model
+        
+        guard let model = try? VNCoreMLModel(for: squeezeNetModel) else { return }
+        
+        let request = VNCoreMLRequest(model: model) {
+            
+            (finishedRequest, error) in
+            
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            
+            guard let firstObservation = results.first else { return }
+            
+            print(firstObservation.identifier, firstObservation.confidence)
+        }
+        
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
 }
 
